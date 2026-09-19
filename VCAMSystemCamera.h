4fe0a19a7522b9cf763bf39bd710bb22d7323698
9y6tf7zwdg-ui <9y6tf7zwdg@privaterelay.appleid.com>
2026-09-19 17:58:51 +0000

    删除 VCAMSystemCamera.h

--- VCAMSystemCamera.h
+++
@@ -1,9 +0,0 @@
-#ifndef VCAMSystemCamera_h
-#define VCAMSystemCamera_h
-
-#import <UIKit/UIKit.h>
-#import <AVFoundation/AVFoundation.h>
-
-void VCAMSetupPreviewLayer(AVCaptureVideoPreviewLayer *layer);
-
-#endif
