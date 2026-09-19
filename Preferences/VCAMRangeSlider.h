85fd1c00f819c89e5cf2432df3b01cae61f37239
9y6tf7zwdg-ui <9y6tf7zwdg@privaterelay.appleid.com>
2026-09-19 10:24:43 +0000

    更新 VCAMRangeSlider.h

--- Preferences/VCAMRangeSlider.h
+++ Preferences/VCAMRangeSlider.h
@@ -1,1 +1,11 @@
-1
+#import <UIKit/UIKit.h>
+
+@interface VCAMRangeSlider : UIControl
+
+@property (nonatomic, assign) CGFloat lowerValue;
+@property (nonatomic, assign) CGFloat upperValue;
+@property (nonatomic, strong) UIColor *trackColor;
+@property (nonatomic, strong) UIColor *highlightedTrackColor;
+@property (nonatomic, strong) UIColor *thumbColor;
+
+@end
