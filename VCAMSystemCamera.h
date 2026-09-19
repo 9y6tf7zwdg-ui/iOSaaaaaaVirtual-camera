a90dcd57c3e32346416b260f896929014cce7b05
9y6tf7zwdg-ui <9y6tf7zwdg@privaterelay.appleid.com>
2026-09-19 11:52:57 +0000

    更新 VCAMSystemCamera.h

--- VCAMSystemCamera.h
+++ VCAMSystemCamera.h
@@ -1,1 +1,10 @@
-1
+#ifndef VCAMSystemCamera_h
+#define VCAMSystemCamera_h
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+// 供 Tweak.x 调用：把替换层安装到 AVCaptureVideoPreviewLayer 上
+void VCAMSetupPreviewLayer(AVCaptureVideoPreviewLayer *layer);
+
+#endif
